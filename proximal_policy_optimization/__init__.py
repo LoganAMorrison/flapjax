2fc1691a1ef51201949d5c7ac6a1c2ba9c394975
Logan A. Morrison <loganmorrison99@gmail.com>
2022-04-11 19:47:03 -0700

    Adding exports

--- proximal_policy_optimization/__init__.py
+++ proximal_policy_optimization/__init__.py
@@ -1,11 +1,18 @@
 from .config import PPOConfig
 from .models import ActorCriticCnn, ActorCriticMlp
-from .train import train, train_from_checkpoint
+from .train import PPOTrainState, train_step, evaluate_model
+from .env import FrameSkip, env_reset, env_step
+from .trajectory import create_trajectory
 
 __all__ = [
     "PPOConfig",
     "ActorCriticMlp",
     "ActorCriticCnn",
-    "train",
-    "train_from_checkpoint",
+    "PPOTrainState",
+    "train_step",
+    "evaluate_model",
+    "FrameSkip",
+    "env_reset",
+    "env_step",
+    "create_trajectory",
 ]
